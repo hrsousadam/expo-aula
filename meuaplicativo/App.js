989062ad8444529def4a615c5bbbba4783e7d896
hrsousadam <hrsousa.dam@gmail.com>
2024-01-12 18:42:37 +0100

    commit_01

--- meuaplicativo/App.js
+++ meuaplicativo/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Meu Primero App para celular rodando em Iphone!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
